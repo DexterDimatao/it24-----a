2f836838a3bb20ca09bc9d25fad39ed687ad4338
dexterdimatao <sonicdexter12@gmail.com>
2024-10-01 09:49:00 +0800

    Added Function to determine if a number is odd or even

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ let name = "Alice";
 let isActive = true; 
 let numbersArray = [1, 2, 3, 4, 5]; 
 let user = { name: "Alice", age: 30 }; 
+// Function to determine if a number is odd or even
+function isOddOrEven(num) {
+    if (num % 2 === 0) {
+        return `${num} is even.`;
+    } else {
+        return `${num} is odd.`;
+    }
+}
