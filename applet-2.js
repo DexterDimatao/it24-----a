a00514fdb0dab010293afa609f5dab4ba1f81930
dexterdimatao <DexterDimatao>
2024-10-06 23:52:13 +0800

    Added initileLayer

--- applet-2.js
+++ applet-2.js
@@ -4,3 +4,9 @@ class LeafletMap {
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
     }
+    initTileLayer() {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+    }
