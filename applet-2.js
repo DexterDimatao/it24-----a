ae294df8e3fb6bb724116a3accd9b2e665b2db70
dexterdimatao <DexterDimatao>
2024-10-06 23:55:11 +0800

    Added loadMarkers

--- applet-2.js
+++ applet-2.js
@@ -14,3 +14,24 @@ class LeafletMap {
         const marker = L.marker([lat, lng]).addTo(this.map);
         marker.bindPopup(message);
     }
+    loadMarkersFromJson(url) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(marker => {
+                    this.addMarker(marker.latitude, marker.longitude, marker.message);
+                });
+            })
+            .catch(error => console.error('Error loading markers:', error));
+    }
+}
+
+const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
+
+
+myMap.addMarker(8.359735, 124.869206, 'CCS Faculty Office');
+myMap.addMarker(8.359639,124.869179, 'CCS Laboratory 1');
+myMap.addMarker(8.359554,124.869153, 'CCS Laboratory 2');
+
+
+myMap.loadMarkersFromJson('applet-2.json');
