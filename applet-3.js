fffb88e915a29d13d806c742eac52b8619dd5831
dexterdimatao <DexterDimatao>
2024-10-07 00:30:28 +0800

    Added card container

--- applet-3.js
+++ applet-3.js
@@ -18,3 +18,21 @@ class DataLogger {
         this.loggedData = []; 
         this.updateCardContainer(); 
     }
+    updateCardContainer() {
+        this.cardContainer.innerHTML = '';
+
+        this.loggedData.forEach(data => {
+            
+            const card = document.createElement('div');
+            card.className = 'card mb-2';
+            card.innerHTML = `
+                <div class="card-body">
+                    <h5 class="card-title">Logged Data</h5>
+                    <p class="card-text">${data}</p>
+                </div>
+            `;
+            this.cardContainer.appendChild(card);
+        });
+        
+        this.displayLogCount();
+    }
