48b11eab72bb8ad864269fda05f9b12035bbb75c
DimataoDexter <sonicdexter12@gmail.com>
2024-10-11 01:29:38 +0800

    Added dev-DynamicCards

--- applet-4.js
+++ applet-4.js
@@ -20,36 +20,4 @@ class StudentList {
             console.error('Error fetching data:', error);
         }
     }
-    renderStudentList(students) {
-        const studentListContainer = document.getElementById('studentList'); 
-        studentListContainer.innerHTML = students.map(student => 
-            `<button class="btn btn-primary" style="margin-top:15px; 
-                                                    width:25rem">
-                ${student.student_name} | ${student.student_program}
-            </button><br>`
-        ).join('');
-    }
-    bindSearchEvent() {
-        const studentSearchBar = document.getElementById('studentSearchBar');
-        const studentSearchListContainer = document.getElementById('studentSearchList');
-
-        studentSearchBar.addEventListener('input', () => {
-            this.filterStudents(studentSearchBar.value, studentSearchListContainer);
-        });
-
-        this.renderStudentList(this.students, studentSearchListContainer);
-    }
-    filterStudents(query, searchListContainer) {
-        const filteredStudents = this.students.filter(student => {
-            const fullName = `${student.student_name} ${student.student_program}`;
-            return fullName.toLowerCase().includes(query.toLowerCase());
-        });
-
-        searchListContainer.innerHTML = '';
-
-        this.renderStudentList(filteredStudents, searchListContainer);
-    }
     
-}
-
-const studentList = new StudentList('applet-4.json');
