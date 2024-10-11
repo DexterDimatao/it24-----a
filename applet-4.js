5bba30fef4ffcf0ab67e3e2aa1bfd7ec60e4c0f4
DimataoDexter <sonicdexter12@gmail.com>
2024-10-11 09:44:31 +0800

    Added Studentlist

--- applet-4.js
+++ applet-4.js
@@ -12,11 +12,3 @@ class StudentList {
         this.bindSearchEvent();
     }
 
-    async fetchData() {
-        try {
-            const response = await fetch(this.dataUrl);
-            this.students = await response.json();
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    }
