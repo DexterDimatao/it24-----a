1953d9c6832480a9d12ab7a18b7d95378c56abb2
DimataoDexter <sonicdexter12@gmail.com>
2024-10-11 09:48:49 +0800

    Added bindersearchEvent

--- applet-4.js
+++ applet-4.js
@@ -35,3 +35,13 @@ class StudentList {
             </button><br>`
         ).join('');
     }
+    bindSearchEvent() {
+        const studentSearchBar = document.getElementById('studentSearchBar');
+        const studentSearchListContainer = document.getElementById('studentSearchList');
+
+        studentSearchBar.addEventListener('input', () => {
+            this.filterStudents(studentSearchBar.value, studentSearchListContainer);
+        });
+
+        this.renderStudentList(this.students, studentSearchListContainer);
+    }
