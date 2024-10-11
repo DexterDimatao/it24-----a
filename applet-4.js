58abf6732410f67ec5399d8899b601b02afbbef3
DimataoDexter <sonicdexter12@gmail.com>
2024-10-11 09:47:31 +0800

    Added fetchData

--- applet-4.js
+++ applet-4.js
@@ -16,4 +16,13 @@ class StudentList {
         await this.fetchData();
         this.renderStudentList(this.students); 
         this.bindSearchEvent();
+    
+    }
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            this.students = await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
     }
