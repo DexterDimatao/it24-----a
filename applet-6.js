db9421041a3cfba6238dcead50cd0ed16ba56694
DimataoDexter <sonicdexter12@gmail.com>
2024-11-02 14:11:17 +0800

    Added updateTask

--- applet-6.js
+++ applet-6.js
@@ -42,4 +42,7 @@ class TodoList {
         const buttons = taskItem.querySelectorAll('button');
         buttons.forEach(button => button.disabled = true);
     }
-    
+    updateTask(taskText) {
+        this.todoList.children[this.editingIndex].querySelector('.task-text').textContent = taskText;
+        this.resetEditing();
+    }
