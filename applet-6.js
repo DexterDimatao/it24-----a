286c5a0922591bfae0f2d4a8239965a7ced61de4
DimataoDexter <sonicdexter12@gmail.com>
2024-11-02 14:12:38 +0800

    Added editTask

--- applet-6.js
+++ applet-6.js
@@ -49,3 +49,9 @@ class TodoList {
     removeTask(event) {
         this.todoList.removeChild(event.target.closest('.todo-item'));
     }
+    editTask(event) {
+        const taskItem = event.target.closest('.todo-item');
+        this.todoInput.value = taskItem.querySelector('.task-text').textContent;
+        this.editingIndex = Array.from(this.todoList.children).indexOf(taskItem);
+        this.addButton.textContent = 'Update';
+    }
