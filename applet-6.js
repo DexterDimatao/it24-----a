d95e713304cf81208bbd44a978a5ca5804798cdd
DimataoDexter <sonicdexter12@gmail.com>
2024-11-02 14:11:59 +0800

    Added removeTask

--- applet-6.js
+++ applet-6.js
@@ -46,3 +46,6 @@ class TodoList {
         this.todoList.children[this.editingIndex].querySelector('.task-text').textContent = taskText;
         this.resetEditing();
     }
+    removeTask(event) {
+        this.todoList.removeChild(event.target.closest('.todo-item'));
+    }
